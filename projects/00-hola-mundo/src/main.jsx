f5f93898569f6f5c40841055438ddf1f28c33ea2
Luis Correal Viveros <luchocorealviveros@gmail.com>
2023-10-10 21:13:51 -0500

    first commit

---
+++ projects/00-hola-mundo/src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { App } from './App'
+import './index.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+// * PascalCase
+// * camelCase
+// * snake_case
+// * kebab-case
+
+// ! los componentes deben ser PascalCase
+
+
+
+root.render(
+  <App/>
+)
